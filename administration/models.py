57c87991fd796909b190fa62800c5a1d3aebbdb6
SegundoMedina <60756741+SegundoMedina@users.noreply.github.com>
2024-11-26 22:01:13 -0300

    Personalizacion django admin

--- administration/models.py
+++ administration/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Provider(models.Model):
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    main_photo = models.ImageField(upload_to="provider/", blank=True, null=True)
-    description = models.TextField()
+    first_name = models.CharField(max_length=128, verbose_name="Nombres")
+    last_name = models.CharField(max_length=128, verbose_name="Apellidos")
+    main_photo = models.ImageField(upload_to="provider/", blank=True, null=True, verbose_name="Selecciona tu foto")
+    description = models.TextField(verbose_name="Describe al paseador")
+
+class Meta:
+    verbose_name_plural = "Paseadores"
+    verbose_name = "Paseador"
