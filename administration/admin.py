a04e07237426fa2ddaa63b4bff88fb6090fa2e8f
SegundoMedina <60756741+SegundoMedina@users.noreply.github.com>
2024-11-27 21:58:59 -0300

    Incluidos Customer + Pet

--- administration/admin.py
+++ administration/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Provider
+from .models import Provider, Customer, Pet
 from django.utils.html import format_html
 
 # Register your models here.
@@ -9,19 +9,64 @@ admin.site.index_title = "Patitas - Dashboard"
 
 @admin.register(Provider)
 class ProviderAdmin(admin.ModelAdmin):
+
     list_display = ('identificador', 'nombres', 'apellidos', )
 
-@admin.display(description="Apellidos")
-def apellidos(self, obj):
-    return obj.last_name
+    @admin.display(description="Apellidos")
+    def apellidos(self, obj):
+        return obj.last_name
 
-@admin.display(description="Nombres")
-def nombres(self, obj):
-    return obj.first_name
+    @admin.display(description="Nombres")
+    def nombres(self, obj):
+        return obj.first_name
 
-@admin.display(description="Identificador")
-def identificador(self, obj):
-    if obj.main_photo:
-        return format_html('<img src={} style="width: 64px;" /><br />' + str(obj.id), 
-                            obj.main_photo.url)
-    return obj.id
+    @admin.display(description="Identificador")
+    def identificador(self, obj):
+        if obj.main_photo:
+            return format_html('<img src={} style="width: 64px;" /><br />' + str(obj.id), 
+                                obj.main_photo.url)
+        return obj.id
+
+class PetInline(admin.TabularInline):
+    model = Pet
+    extra = 1
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    #pass
+
+    list_display = ('identificador', 'nombres', 'apellidos', 'direccion',)
+    inlines = [PetInline] #<-----
+
+    @admin.display(description="Identificador")
+    def identificador(self, obj):
+        return obj.id
+
+    @admin.display(description="Nombres")
+    def nombres(self, obj):
+        return obj.first_name
+    
+    @admin.display(description="Apellidos")
+    def apellidos(self, obj):
+        return obj.last_name
+    
+    @admin.display(description="Direccion")
+    def direccion(self, obj):
+        return obj.address
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    #pass
+
+    list_display = ('nombre','foto')
+
+    @admin.display(description='Nombre')
+    def nombre(self, obj):
+        return obj.name
+    
+    @admin.display(description="Foto")
+    def foto(self, obj):
+        if obj.main_photo:
+            return format_html('<img src={} style="width: 64px;" /><br />' + str(obj.id), 
+                                obj.main_photo.url)
+        return obj.id
--- administration/models.py
+++ administration/models.py
@@ -7,6 +7,29 @@ class Provider(models.Model):
     main_photo = models.ImageField(upload_to="provider/", blank=True, null=True, verbose_name="Selecciona tu foto")
     description = models.TextField(verbose_name="Describe al paseador")
 
-class Meta:
-    verbose_name_plural = "Paseadores"
-    verbose_name = "Paseador"
+    class Meta:
+        verbose_name_plural = "Paseadores"
+        verbose_name = "Paseador"
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=128, verbose_name="Nombres")
+    last_name = models.CharField(max_length=128, verbose_name="Apellidos")
+    address = models.CharField(max_length=128, verbose_name="Direccion")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name_plural = "Clientes"
+        verbose_name = "Cliente"
+
+class Pet(models.Model):
+    customer= models.ForeignKey(Customer, on_delete=models.RESTRICT, verbose_name="Cliente") # Llave Foranea
+    name = models.CharField(max_length=128, verbose_name="Nombre")
+    age = models.CharField(max_length=64, verbose_name="Edad")
+    pedigree = models.CharField(max_length=128, verbose_name="Raza")
+    main_photo = models.ImageField(upload_to="pets/", blank=True, null=True, verbose_name="Selecciona tu foto")
+
+    class Meta:
+        verbose_name_plural = "Mascotas"
+        verbose_name = "Mascota"
